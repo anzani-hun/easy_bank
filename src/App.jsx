611eb3cfc78eb928270a71b8d06d61ac9308d4ba
anzani-hun <anzani@hun>
2024-03-18 17:08:51 +0100

    oldalak_kész

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-// ES7 kiegészítő kell és RAFCE 
+// ES7 kiegészítő kell és RAFCE szót akkor kitölti.
 
 
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import WhyUs from './components/WhyUs'
+import Latest from './components/Latest'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <WhyUs />
+      <Latest />
+      <Footer />
+    </div>
   )
 }
 
