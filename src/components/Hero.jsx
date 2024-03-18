6916ce1b920b154289f4fa329c4c16d3d7a35122
anzani-hun <anzani@hun>
2024-03-18 18:04:11 +0100

    hero szunet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="hero">
+        <div className='wrapper'>
+            <div>
+                <h1>Next Generation Digital Banking</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sequi, pariatur, repellat cum quam mollitia provident beatae laboriosam voluptates fugiat commodi illo praesentium non possimus enim.</p>
+                <button>Request Invite</button>
+            </div>
+        </div>
+    </section>
   )
 }
 
