a0f7c631595a790fe2bbe510c449c822d8a2d718
anzani-hun <anzani@hun>
2024-03-18 17:35:27 +0100

    menu kesz

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='wrapper'>
+        <span className='logo'>easybank</span>
+        
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+
+        <button>Request Invite</button>
+      </div>
+    </nav>
   )
 }
 
