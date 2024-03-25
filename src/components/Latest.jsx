acd4df100c9269b81ca1ae2bc9bdee4dbaee1dfd
anzani-hun <anzani@hun>
2024-03-25 18:41:08 +0100

    kepek kész

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -16,13 +16,27 @@ const Latest = () => {
         <h2>Latest</h2>
         <div className="cards">
 
-        { apiData.map(data => <div>{data.title}</div>) }
+          {apiData.map((data, idx) => <LatestCard key={idx} {...data} />)}
 
         </div>
-
       </div>
     </section>
   )
 }
+
+
+const LatestCard = (props) => {
+  return (
+    <div className='latest-card'>
+
+    <img src={"http://localhost:8000/" + props.img} alt="{props.title}" />
+    <div>
+      <h6>{props.creator}</h6>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  </div>)
+}
+
 
 export default Latest
