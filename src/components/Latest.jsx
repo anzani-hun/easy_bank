cdefdeb85eded81632be34e76b6c2f09442f7e76
anzani-hun <anzani@hun>
2024-03-25 17:27:14 +0100

    latest json kész

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+// API FETCHELÉSE ez fontos
 const Latest = () => {
+  const [apiData, setApiData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/latest.json")
+      .then(res => res.json())
+      .then(data => setApiData(data))
+  }, [])
+
   return (
-    <div>Latest</div>
+    <section id="latest">
+      <div className="wrapper">
+        <h2>Latest</h2>
+        <div className="cards">
+
+        { apiData.map(data => <div>{data.title}</div>) }
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
